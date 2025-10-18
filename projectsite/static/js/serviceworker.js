e6989137d7bb54dd60d1fb10cdd5820869744244
Calaisha Mae S. Ducao <itscalaisha@gmail.com>
2025-10-19 03:51:33 +0800

    fixed serviceworker.js

---
+++ projectsite/static/js/serviceworker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('projectsite-cache-v1').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/static/css/bootstrap.min.css',
+                '/static/js/core/bootstrap.min.js',
+                '/static/js/core/popper.min.js',
+                '/static/js/core/jquery.3.2.1.min.js',
+                '/static/css/style.css',
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
